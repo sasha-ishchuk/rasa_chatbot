3d0d3d4347aee8e53a0a22a3623e875651ecb262
sasha <alexa.ishchuk@gmail.com>
2023-12-06 22:12:57 +0100

    basic chatbot created

---
+++ actions/actions.py
@@ -0,0 +1,58 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+
+import json
+from pathlib import Path
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class CheckOpeningHours(Action):
+    opening_hours = json.loads(Path("data/opening_hours.json").read_text())
+
+    def name(self) -> Text:
+        return "action_check_opening_hours"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        entities = tracker.latest_message.get('entities', [])
+
+        for entity in entities:
+            if entity['entity'] == 'day_of_week':
+                day = entity['value']
+                if day in self.opening_hours['items']:
+                    dispatcher.utter_message(f"Yes, the restaurant is open on {day}.")
+                else:
+                    dispatcher.utter_message(f"Sorry, I don't have information about the opening hours for {day}.")
+
+        return []
+

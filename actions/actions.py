a953b826315e3818fa14cc8510069db4786b4624
sasha <alexa.ishchuk@gmail.com>
2023-12-06 22:41:36 +0100

    add and handle check_opening_hours_at_time action

--- actions/actions.py
+++ actions/actions.py
@@ -56,3 +56,33 @@ class CheckOpeningHours(Action):
 
         return []
 
+
+class CheckOpeningHoursAtTime(Action):
+    opening_hours = json.loads(Path("data/opening_hours.json").read_text())
+
+    def name(self) -> Text:
+        return "action_check_opening_hours_at_time"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        entities = tracker.latest_message.get('entities', [])
+
+        for entity in entities:
+            if entity['entity'] == 'day_of_week':
+                day = entity['value']
+                if day in self.opening_hours['items']:
+                    user_time = int(tracker.get_slot('time'))
+
+                    opening_time = self.opening_hours['items'][day]['open']
+                    closing_time = self.opening_hours['items'][day]['close']
+
+                    if opening_time <= user_time <= closing_time:
+                        dispatcher.utter_message(f"Yes, the restaurant is open on {day} at {user_time}:00.")
+                    else:
+                        dispatcher.utter_message(f"Sorry, the restaurant is closed on {day} at {user_time}:00.")
+                else:
+                    dispatcher.utter_message(f"Sorry, I don't have information about the opening hours for {day}.")
+
+        return []

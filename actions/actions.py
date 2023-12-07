91b61c8a1d163365896285323a38efde4bb47bb2
sasha <alexa.ishchuk@gmail.com>
2023-12-07 19:57:02 +0100

    add and handle action_place_order and action_place_order_default_one_item

--- actions/actions.py
+++ actions/actions.py
@@ -29,9 +29,13 @@
 
 import json
 from pathlib import Path
-from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List, Tuple
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import logging
+
+# Create a logger
+logger = logging.getLogger(__name__)
 
 
 class CheckOpeningHours(Action):
@@ -112,3 +116,80 @@ class ListMenu(Action):
             dispatcher.utter_message(text="Sorry, the menu is not available at the moment.")
 
         return []
+
+
+class ActionPlaceOrder(Action):
+
+    menu_data = json.loads(Path("data/menu.json").read_text())
+
+    def name(self) -> Text:
+        return "action_place_order"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Extract entities from user input
+        quantity = int(tracker.get_slot("quantity"))
+        dish_item = tracker.get_slot("dish_item")
+
+        # Your logic for processing the order
+        if dish_item:
+            total_price, total_time = self.calculate_order(dish_item, quantity)
+            dispatcher.utter_message(f"The order for {quantity} {dish_item}(s) is placed. Total price: ${total_price}. \n"
+                                     f"Time to prepare: {total_time} hour(s). We'll be waiting you pick-up your order!")
+
+        return []
+
+    def calculate_order(self, item: Text, quantity: int) -> Tuple[float, float]:
+        # Load menu data from menu.json file
+        # with open('menu.json', 'r') as file:
+        #     menu_data = json.load(file)
+
+        # Initialize variables for total price and total time
+        total_price = 0
+        total_time = 0
+
+        # Search for the selected item in the menu and calculate total price and time
+        for menu_item in self.menu_data['items']:
+            if menu_item['name'].lower() == item.lower():
+                item_price = menu_item['price']
+                preparation_time = menu_item['preparation_time']
+
+                # Calculate total price and total time based on quantity
+                total_price += item_price * quantity
+                total_time += preparation_time * quantity
+
+                break  # Stop loop once the item is found
+
+        return total_price, total_time
+
+
+class ActionPlaceOrderDefaultOneItem(Action):
+
+    menu_data = json.loads(Path("data/menu.json").read_text())
+
+    def name(self) -> Text:
+        return "action_place_order_default_one_item"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Extract entities from user input
+        dish_item = tracker.get_slot("dish_item")
+
+        quantity = 1
+
+        if dish_item:
+            total_price, total_time = self.calculate_order(dish_item)
+            dispatcher.utter_message(f"The order for {quantity} {dish_item}(s) is placed. Total price: ${total_price}. \n"
+                                     f"Time to prepare: {total_time} hour(s). We'll be waiting you pick-up your order!")
+        return []
+
+    def calculate_order(self, item: Text) -> Tuple[float, float]:
+        for menu_item in self.menu_data['items']:
+            if menu_item['name'].lower() == item.lower():
+                item_price = menu_item['price']
+                preparation_time = menu_item['preparation_time']
+                return item_price, preparation_time

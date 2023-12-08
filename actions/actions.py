17f81a91e57b8214964bfb649a8bbc4496dd0c01
sasha <alexa.ishchuk@gmail.com>
2023-12-08 18:16:36 +0100

    add more custom actions

--- actions/actions.py
+++ actions/actions.py
@@ -6,8 +6,10 @@
 
 import json
 from pathlib import Path
+from datetime import datetime
 from typing import Any, Text, Dict, List, Tuple
 from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
 
@@ -65,6 +67,37 @@ class CheckOpeningHoursAtTime(Action):
         return []
 
 
+class CheckOpenNow(Action):
+    opening_hours = json.loads(Path("data/opening_hours.json").read_text())
+
+    def name(self) -> Text:
+        return "action_check_open_now"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        current_datetime = datetime.now()
+        day_of_week = current_datetime.weekday()
+        days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+        day = days_of_week[day_of_week]
+        user_time = int(current_datetime.hour)
+
+        if day in self.opening_hours['items']:
+            opening_time = self.opening_hours['items'][day]['open']
+            closing_time = self.opening_hours['items'][day]['close']
+
+            if opening_time <= user_time <= closing_time:
+                dispatcher.utter_message(text="Yes, the restaurant is open now.")
+            else:
+                dispatcher.utter_message(text="Sorry, the restaurant is closed now.")
+        else:
+            dispatcher.utter_message(text="Sorry, I don't have information about the opening hours for now.")
+
+        return []
+
+
 class ListMenu(Action):
     menu_data = json.loads(Path("data/menu.json").read_text())
 
@@ -92,7 +125,6 @@ class ListMenu(Action):
 
 
 class ActionPlaceOrder(Action):
-
     menu_data = json.loads(Path("data/menu.json").read_text())
 
     def name(self) -> Text:
@@ -107,8 +139,9 @@ class ActionPlaceOrder(Action):
 
         if dish_item:
             total_price, total_time = self.calculate_order(dish_item, quantity)
-            dispatcher.utter_message(f"The order for {quantity} {dish_item}(s) is placed. Total price: ${total_price}. \n"
-                                     f"Time to prepare: {total_time} hour(s). We'll be waiting you pick-up your order!")
+            dispatcher.utter_message(
+                f"The order for {quantity} {dish_item}(s) is placed. Total price: ${total_price}. \n"
+                f"Time to prepare: {total_time} hour(s). \n")
 
         return []
 
@@ -129,7 +162,6 @@ class ActionPlaceOrder(Action):
 
 
 class ActionPlaceOrderDefaultOneItem(Action):
-
     menu_data = json.loads(Path("data/menu.json").read_text())
 
     def name(self) -> Text:
@@ -144,8 +176,9 @@ class ActionPlaceOrderDefaultOneItem(Action):
 
         if dish_item:
             total_price, total_time = self.calculate_order(dish_item)
-            dispatcher.utter_message(f"The order for {quantity} {dish_item}(s) is placed. Total price: ${total_price}. \n"
-                                     f"Time to prepare: {total_time} hour(s). We'll be waiting you pick-up your order!")
+            dispatcher.utter_message(
+                f"The order for {quantity} {dish_item}(s) is placed. Total price: ${total_price}. \n"
+                f"Time to prepare: {total_time} hour(s). \n")
         return []
 
     def calculate_order(self, item: Text) -> Tuple[float, float]:
@@ -154,3 +187,39 @@ class ActionPlaceOrderDefaultOneItem(Action):
                 item_price = menu_item['price']
                 preparation_time = menu_item['preparation_time']
                 return item_price, preparation_time
+
+
+class ConfirmAdds(Action):
+    extra_keywords = Path("data/txt/extra_keywords.txt").read_text().split("\n")
+    without_keywords = Path("data/txt/without_keywords.txt").read_text().split("\n")
+    available_extras = Path("data/txt/available_extras.txt").read_text().split("\n")
+
+    def name(self) -> Text:
+        return "action_confirm_adds"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        add_type = tracker.get_slot("type_of_extra_request")
+        extra_item = tracker.get_slot("extra_item")
+
+        add = None
+        if add_type in self.extra_keywords:
+            add = True
+        elif add_type in self.without_keywords:
+            add = False
+
+        item_present = False
+        if extra_item in self.available_extras:
+            item_present = True
+
+        if add is None and not item_present:
+            dispatcher.utter_message("Sorry... I don't understood your needs :(")
+        elif item_present and add is not None:
+            dispatcher.utter_message(f"Your order updated. Added note: '{add_type} {extra_item}'.\n")
+        elif not item_present:
+            dispatcher.utter_message("Sorry... We don't have this extra item")
+
+        return []
+
